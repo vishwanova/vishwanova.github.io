27e13c9409ba19fa27255dd44069c038c439c9c5
vishwanova <ghostboom2211@gmail.com>
2026-05-21 08:30:52 +0530

    big website chunk creation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
 
+document.querySelectorAll("[data-page]").forEach((link) => {
+  if (link.getAttribute("data-page") === currentPage) {
+    link.classList.add("is-active");
+  }
+});
